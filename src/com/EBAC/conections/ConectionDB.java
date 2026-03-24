e82fd07da6e98ce2383e1ccd34c6f432e5c788c7
Lucas Fernando <115047547+LucasDevsPaula@users.noreply.github.com>
2026-03-24 14:53:38 -0400

    Update database password in ConectionDB.java

--- src/com/EBAC/conections/ConectionDB.java
+++ src/com/EBAC/conections/ConectionDB.java
@@ -8,7 +8,7 @@ public class ConectionDB {
     private static final String URL =
             "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimeZone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Vermelho08@";
+    private static final String PASSWORD = "yourpassword";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
